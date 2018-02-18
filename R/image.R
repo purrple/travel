cc5789b1775ee9665667859083cf661db4fc0e32
Romain Francois <romain@thinkr.fr>
2018-02-18 11:31:29 +0100

    clean

--- R/image.R
+++ R/image.R
@@ -21,7 +21,28 @@ montage4 <- function( src, dest ){
   image_write(img, path = dest, format = "jpg" )
 }
 
-#recuperer le nom du fichier
+#fonction pour redimensionner une image
+process_img <- function(file){
+  if (basename(file) == "header.jpg"){
+    redim_img(file, w=1920)
+  } else {
+    redim_img(file, w=800)
+  }
+}
+
+redim_img <-function(file, w){
+  img <- image_read(file)
+  width <- image_info(img)$width
+  if(width > w){
+    cat( glue("\rredim {file}                                                ") )
+    image_scale(img, w) %>%
+      image_write(path = file, format = "jpg")
+    TRUE
+  } else {
+    cat( glue("\r{file} deja redim                                            ") )
+    FALSE
+  }
+}
 
 redim_all <- function(){
   files <- list.files("static/img", pattern="jpg$", recursive=TRUE, full.names=TRUE)
@@ -32,52 +53,4 @@ redim_all <- function(){
   message(glue("J'ai modifié {n_modif} fichiers sur {n_total}"))
 }
 
-redim_img <-function(file, w){
-  img <- image_read(file)
-  width <- image_info(img)$width
-   if(width > w){
-     cat( glue("\rredim {file}                                                ") )
-     image_scale(img, w) %>%
-       image_write(path = file, format = "jpg")
-     TRUE
-   } else {
-     cat( glue("\r{file} deja redim                                            ") )
-     FALSE
-   }
- }
 
-#fonction pour redimensionner une image
-process_img <- function(file){
-
-  if (basename(file) == "header.jpg"){
-    redim_img(file, w=1920)
-  } else {
-    redim_img(file, w=800)
-    }
-}
-
-re#gif alix qui saute
-img1 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3480.jpg")
-img2 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3481.jpg")
-img3 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3482.jpg")
-img4 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3483.jpg")
-img5 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3484.jpg")
-img6 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3485.jpg")
-
-vect <- c(img1,img2,img3,img4,img5,img6)
-vect <- image_scale(vect, 800)
-gif <- image_animate(vect, fps = 5)
-
-image_write(image = gif, path= "/Users/romain/git/travel/static/img/nyc-2018/times-square/alix.gif", 
-  format = "gif")
-
-#gif
-img1 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3388.jpg")
-img2 <- image_read("/Users/romain/git/travel/static/img/nyc-2018/times-square/IMG_3389.jpg")
-
-vect <- c(img1,img2)
-vect <- image_scale(vect, 800)
-gif <- image_animate(vect, fps = 5)
-
-image_write(image = gif, path= "/Users/romain/git/travel/static/img/nyc-2018/times-square/romain.gif", 
-  format = "gif")
