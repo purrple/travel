4b543fbbf1df530f795c55e24779c4c461f9f837
Romain Francois <romain@thinkr.fr>
2018-02-18 18:54:37 +0100

    seulement reorienter les images qui ont une oriantation differente de 1

--- R/orient_all.R
+++ R/orient_all.R
@@ -3,13 +3,24 @@ library(glue)
 library(magick)
 library(assertthat)
 library(progress)
+library(exiv)
+library(dplyr)
 
 orient_all <- function( files ){
+  
+  orientations <- map_int( files, ~{
+    read_exif(.x) %>% 
+      filter( exif_key == "Exif.Image.Orientation") %>% 
+      pull(exif_val) %>% 
+      as.integer()
+  })
+  
+  
   pb <- progress_bar$new(total = length(files))
   walk( files, ~{
-    image_read(.) %>% 
+    image_read(.x) %>% 
       image_orient() %>% 
-      image_write(path = .)
+      image_write(path = .x)
     pb$tick()
   })
 }
