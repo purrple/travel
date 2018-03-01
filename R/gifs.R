1e74a54f3b492338ca7685dd0d12e4bc9c62b03c
cecilesauder <cecile.sauder@gmail.com>
2018-03-01 18:08:59 +0100

    gif changement

--- R/gifs.R
+++ R/gifs.R
@@ -1,5 +1,26 @@
 library(magick)
 
+#' animation de n images
+#' 
+#' @param src Les images à animer
+
+src = c("/home/cecile/Images/guibb.jpg", "/home/cecile/Images/guibb2.jpg", "/home/cecile/Images/ccbb.jpg") 
+anim <- function(src, vitesse, taille ){
+  n <- length(src)
+  img <- src %>%
+    image_read() %>%
+    image_scale(taille) %>%
+    image_animate(fps = vitesse)
+  img
+}
+
+anim(src, 2, 200)
+
+runGist("https://gist.githubusercontent.com/jeroen/bd1d0a30e7184a5320695ee2bda12c70/raw/ece820dc1b13e5c97e6df48f3792097e09647506/magickshiny.R")
+
+source("https://gist.githubusercontent.com/jeroen/bd1d0a30e7184a5320695ee2bda12c70/raw/ece820dc1b13e5c97e6df48f3792097e09647506/magickshiny.R")
+
+
 img1 <- image_read("static/img/nyc-2018/times-square/IMG_3480.jpg")
 img2 <- image_read("static/img/nyc-2018/times-square/IMG_3481.jpg")
 img3 <- image_read("static/img/nyc-2018/times-square/IMG_3482.jpg")
